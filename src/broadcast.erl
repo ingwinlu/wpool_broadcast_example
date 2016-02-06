192b8bd53a1645adc3f2d2537b8a4e5b47e90a41
derwinlu <derwinlu+git@gmail.com>
2016-02-06 15:39:46 +0100

    Implement broadcast like behaviour
    Use a process group to cast a command to all workers.

--- src/broadcast.erl
+++ src/broadcast.erl
@@ -2,7 +2,7 @@
 -behaviour(gen_server).
 
 %% API.
--export([start_pool/0, bump/0, bump/1]).
+-export([start_pool/0, bump/0, bump/1, bump_all/0]).
 %% gen_server.
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
@@ -10,10 +10,12 @@
 -record(state, {
           counter = 0
 }).
+-define(PG_NAME, broadcast_pg).
 
 %% API.
 
 start_pool() ->
+    pg2:create(?PG_NAME),
     wpool:start_pool(
       ?MODULE, [
                 {workers, 10},
@@ -27,9 +29,18 @@ bump() ->
 bump(Pid) ->
     gen_server:cast(Pid, bump).
 
+bump_all() ->
+    spawn_link(fun
+        () ->
+            Workers = pg2:get_members(?PG_NAME),
+            [broadcast:bump(W) || W <- Workers]
+    end).
+
+
 %%% gen_server.
 
 init(_) ->
+    pg2:join(?PG_NAME, self()),
     {ok, #state{}}.
 
 handle_call(_Request, _From, State) ->
